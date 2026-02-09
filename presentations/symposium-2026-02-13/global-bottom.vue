47f0257d4c464f22dcbd7dfdc2d11b2e7e5fd12a
lee mighdoll <lee@underneath.ca>
2026-02-08 16:14:44 -0800

    better numbering

--- presentations/symposium-2026-02-13/global-bottom.vue
+++ presentations/symposium-2026-02-13/global-bottom.vue
@@ -1,5 +1,8 @@
 <template>
-  <footer class="absolute bottom-0 right-4 text-sm text-gray-400 z-10">
+  <footer
+    v-if="!['cover', 'center'].includes($nav.currentLayout)"
+    class="absolute bottom-1 right-2 text-sm text-gray-400 z-10"
+  >
     <SlideCurrentNo />
   </footer>
 </template>
